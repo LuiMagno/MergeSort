713045c3f7005ca922df7e68889e28825d5bd974
Lui Magno <lui-magno@hotmail.com>
2018-05-27 16:07:07 -0300

    Adicionando cores a ordenação

--- main.cpp
+++ main.cpp
@@ -15,6 +15,12 @@ int last_press_y = 0;
 double pos_z[15];
 double pos_x[15];
 double pos_y[15];
+
+float color1[15];
+float color2[15];
+float color3[15];
+
+
 int passo = 1;
 
 
@@ -76,17 +82,6 @@ void Desenha_Eixos_Coordenados()
 
 }
 
-void Desenha_Face_Superior_Cubo()
-{
-	float lado_cubo = 5;
-	glNormal3f(0.0, 1.0, 0.0); /* Normal da Face */
-	glBegin(GL_POLYGON);
-		glVertex3d(lado_cubo/2, lado_cubo, lado_cubo/2);
-		glVertex3d(-lado_cubo/2, lado_cubo, lado_cubo/2);
-		glVertex3d(-lado_cubo/2, lado_cubo, -lado_cubo/2);
-		glVertex3d(lado_cubo/2, lado_cubo,-lado_cubo/2);
-	glEnd();
-}
 
 void ParametrosIluminacao()
 {
@@ -118,32 +113,32 @@ void ParametrosIluminacao()
 }
 void Desenha_Barra(void){
     glBegin(GL_QUADS);        // Draw The Cube Using quads
-            glColor3f(0.0f,1.0f,0.0f);    // Color Blue
+         //   glColor3f(0.0f,1.0f,0.0f);    // Color Blue
             glVertex3f( 0.2f, 0.2f,-0.2f);    // Top Right Of The Quad (Top)
             glVertex3f(-0.2f, 0.2f,-0.2f);    // Top Left Of The Quad (Top)
             glVertex3f(-0.2f, 0.2f, 0.2f);    // Bottom Left Of The Quad (Top)
             glVertex3f( 0.2f, 0.2f, 0.2f);    // Bottom Right Of The Quad (Top)
-            glColor3f(1.0f,0.5f,0.0f);    // Color Orange
+         //   glColor3f(1.0f,0.5f,0.0f);    // Color Orange
             glVertex3f( 0.2f,-0.2f, 0.2f);    // Top Right Of The Quad (Bottom)
             glVertex3f(-0.2f,-0.2f, 0.2f);    // Top Left Of The Quad (Bottom)
             glVertex3f(-0.2f,-0.2f,-0.2f);    // Bottom Left Of The Quad (Bottom)
             glVertex3f( 0.2f,-0.2f,-0.2f);    // Bottom Right Of The Quad (Bottom)
-            glColor3f(1.0f,0.0f,0.0f);    // Color Red
+        //    glColor3f(1.0f,0.0f,0.0f);    // Color Red
             glVertex3f( 0.2, 0.2, 0.2);    // Top Right Of The Quad (Front)
             glVertex3f(-0.2, 0.2, 0.2);    // Top Left Of The Quad (Front)
             glVertex3f(-0.2,-0.2, 0.2);    // Bottom Left Of The Quad (Front)
             glVertex3f( 0.2,-0.2, 0.2);    // Bottom Right Of The Quad (Front)
-            glColor3f(1.0f,1.0f,0.0f);    // Color Yellow
+       //     glColor3f(1.0f,1.0f,0.0f);    // Color Yellow
             glVertex3f( 0.2f,-0.2f,-0.2f);    // Top Right Of The Quad (Back)
             glVertex3f(-0.2f,-0.2f,-0.2f);    // Top Left Of The Quad (Back)
             glVertex3f(-0.2f, 0.2f,-0.2f);    // Bottom Left Of The Quad (Back)
             glVertex3f( 0.2f, 0.2f,-0.2f);    // Bottom Right Of The Quad (Back)
-            glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+        //    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
             glVertex3f(-0.2f, 0.2f, 0.2f);    // Top Right Of The Quad (Left)
             glVertex3f(-0.2f, 0.2f,-0.2f);    // Top Left Of The Quad (Left)
             glVertex3f(-0.2f,-0.2f,-0.2f);    // Bottom Left Of The Quad (Left)
             glVertex3f(-0.2f,-0.2f, 0.2f);    // Bottom Right Of The Quad (Left)
-            glColor3f(1.0f,0.0f,1.0f);    // Color Violet
+        //    glColor3f(1.0f,0.0f,1.0f);    // Color Violet
             glVertex3f( 0.2f, 0.2f,-0.2f);    // Top Right Of The Quad (Right)
             glVertex3f( 0.2f, 0.2f, 0.2f);    // Top Left Of The Quad (Right)
             glVertex3f( 0.2f,-0.2f, 0.2f);    // Bottom Left Of The Quad (Right)
@@ -160,6 +155,8 @@ void Desenha_Plano(void){
         glVertex3f(-30.0f,0.0f,-30.0f);    // Bottom Left Of The Quad (Bottom)
         glVertex3f( 30.0f,0.0f,-30.0f);    // Bottom Right Of The Quad (Bottom)
   glEnd();
+
+
 
 }
 void Desenha(void)
@@ -199,12 +196,14 @@ void Desenha(void)
     glPushMatrix();
         glScalef(1.0, 6.0, 1.0);
         glTranslatef(pos_x[0], 0.2, pos_z[0]);
+        glColor3d(color1[0], color2[0], color3[0]);
         Desenha_Barra(); // Barra 0
     glPopMatrix();
 
     glPushMatrix();
         glScalef(1.0, 4.0, 1.0);
         glTranslatef(pos_x[1], 0.2, pos_z[1]);
+        glColor3d(color1[1], color2[1], color3[1]);
         Desenha_Barra(); // Barra 1
     glPopMatrix();
 
@@ -212,6 +211,7 @@ void Desenha(void)
 
         glScalef(1.0, 1.0, 1.0);
         glTranslatef(pos_x[2], 0.2, pos_z[2]);
+        glColor3d(color1[2], color2[2], color3[2]);
         Desenha_Barra(); // Barra 2
     glPopMatrix();
 
@@ -219,6 +219,7 @@ void Desenha(void)
 
         glScalef(1.0, 7.0, 1.0);
         glTranslatef( pos_x[3], 0.2, pos_z[3]);
+        glColor3d(color1[3], color2[3], color3[3]);
         Desenha_Barra(); // Barra 3
     glPopMatrix();
 
@@ -226,6 +227,7 @@ void Desenha(void)
 
         glScalef(1.0, 3.0, 1.0);
         glTranslatef(pos_x[4], 0.2, pos_z[4]);
+        glColor3d(color1[4], color2[4], color3[4]);
         Desenha_Barra(); // Barra 4
     glPopMatrix();
 
@@ -233,6 +235,7 @@ void Desenha(void)
 
         glScalef(1.0, 10.0, 1.0);
         glTranslatef(pos_x[5], 0.2, pos_z[5]);
+        glColor3d(color1[5], color2[5], color3[5]);
         Desenha_Barra(); // Barra 5
     glPopMatrix();
 
@@ -240,6 +243,7 @@ void Desenha(void)
 
         glScalef(1.0, 5.0, 1.0);
         glTranslatef(pos_x[6], 0.2, pos_z[6]);
+        glColor3d(color1[6], color2[6], color3[6]);
         Desenha_Barra(); // Barra 6
     glPopMatrix();
 
@@ -247,12 +251,14 @@ void Desenha(void)
 
         glScalef(1.0, 10.0, 1.0);
         glTranslatef(pos_x[7], 0.2, pos_z[7]);
+        glColor3d(color1[7], color2[7], color3[7]);
         Desenha_Barra(); // Barra 7
     glPopMatrix();
 
 	glPushMatrix();
-
+        glScaled(1, 4.0, 1.0);
         glTranslatef(pos_x[8], 0.2, pos_z[8]);
+        glColor3d(color1[8], color2[8], color3[8]);
         Desenha_Barra(); // Barra 8
 	glPopMatrix();
 
@@ -260,7 +266,7 @@ void Desenha(void)
 
         glScalef(1.0, 10.0, 1.0);
         glTranslatef(pos_x[9], 0.2,pos_z[9]);
-
+        glColor3d(color1[9], color2[9], color3[9]);
         Desenha_Barra(); // Barra 9
     glPopMatrix();
 
@@ -268,6 +274,7 @@ void Desenha(void)
 
        glScalef(1.0, 5.0, 1.0);
        glTranslatef(pos_x[10], 0.2, pos_z[10]);
+       glColor3d(color1[10], color2[10], color3[10]);
        Desenha_Barra(); // Barra 10
    glPopMatrix();
 
@@ -275,34 +282,39 @@ void Desenha(void)
 
         glScalef(1.0, 8.0, 1.0);
         glTranslatef(pos_x[11], 0.2, pos_z[11]);
+        glColor3d(color1[11], color2[11], color3[11]);
         Desenha_Barra(); // Barra 11
     glPopMatrix();
 
      glPushMatrix();
 
-        glScalef(1.0, 3.0, 1.0);
+        glScalef(1.0, 7.0, 1.0);
         glTranslatef(pos_x[12], 0.2, pos_z[12]);
+        glColor3d(color1[12], color2[12], color3[12]);
         Desenha_Barra(); // Barra 12
     glPopMatrix();
 
      glPushMatrix();
 
-        glScalef(1.0, 2.0, 1.0);
+        glScalef(1.0, 9.0, 1.0);
         glTranslatef(pos_x[13], 0.2, pos_z[13]);
+        glColor3d(color1[13], color2[13], color3[13]);
         Desenha_Barra(); // Barra 13
     glPopMatrix();
 
      glPushMatrix();
 
-        glScalef(1.0, 2.0, 1.0);
+        glScalef(1.0, 12.0, 1.0);
         glTranslatef(pos_x[14], 0.2, pos_z[14]);
+        glColor3d(color1[14], color2[14], color3[14]);
         Desenha_Barra(); // Barra 14
     glPopMatrix();
 
      glPushMatrix();
 
-        glScalef(1.0, 1.0, 1.0);
+        glScalef(1.0, 13.0, 1.0);
         glTranslatef(pos_x[15], 0.2, pos_z[15]);
+        glColor3d(color1[15], color2[15], color3[15]);
         Desenha_Barra(); // Barra 15
     glPopMatrix();
 
@@ -333,9 +345,10 @@ void Anima(int value)  /* Usada quando se usar glutTimerFunc() */
  if( pos_z[0]<0 && passo == 1){
     for(int i = 0; i<16; i++){
         pos_z[i] += 0.05;
-    }
+        }
     if(pos_z[15] >= 0){
          passo = 2;
+
     }
 
 
@@ -344,7 +357,11 @@ void Anima(int value)  /* Usada quando se usar glutTimerFunc() */
     if(pos_z[0]<2 && passo == 2){
         for(int i = 0; i<8; i++){
             pos_z[i] +=0.05;
+            color1[i] = 1;
+            color2[i] = 0;
+            color3[i] = 0;
         }
+
         if(pos_z[0] >= 2){
             passo = 3;
         }
@@ -353,6 +370,9 @@ void Anima(int value)  /* Usada quando se usar glutTimerFunc() */
     if(pos_z[0]<4 && passo == 3){
         for(int i = 0; i<4; i++){
             pos_z[i] +=0.05;
+            color1[i] = 1;
+            color2[i] = 1;
+            color3[i] = 0;
         }
         if(pos_z[0] >= 4){
             passo = 4;
@@ -361,7 +381,11 @@ void Anima(int value)  /* Usada quando se usar glutTimerFunc() */
     if(pos_z[0]<6 && passo == 4){
         for(int i = 0; i<2; i++){
             pos_z[i] +=0.05;
+            color1[i] = 1;
+            color2[i] = 0;
+            color3[i] = 1;
         }
+
         if(pos_z[0] >= 6){
             passo = 5;
         }
@@ -497,29 +521,24 @@ void Anima(int value)  /* Usada quando se usar glutTimerFunc() */
             passo = 21;
         }
     }
-    if(pos_x[3]<-4.0  && passo == 21){
-            pos_x[3] += 0.02;
-            pos_x[0] -= 0.02;
-        if(pos_x[3]>=-4.0){
-            passo = 22;
-        }
-    }
-    if(pos_x[3]<-3.0 && passo == 22){
+    if(pos_x[3]<-3.0  && passo == 21){
             pos_x[3] += 0.02;
             pos_x[6] -= 0.02;
         if(pos_x[3]>=-3.0){
+            passo = 22;
+        }
+    }
+    if(pos_z[0]>0  && passo == 22){
+        for(int i = 0; i<8; i++){
+            pos_z[i] -=0.05;
+        }
+         if(pos_x[0]>=0){
             passo = 23;
         }
     }
-    if(pos_x[0]<-4.0  && passo == 23){
-            pos_x[0] += 0.02;
-            pos_x[6] -= 0.02;
-        if(pos_x[0]>=-4.0){
-            passo = 24;
-        }
-    }
+
 	glutPostRedisplay();
-	glutTimerFunc(10, Anima, 1);
+	glutTimerFunc(30, Anima, 1);
 }
 /* Callback chamada quando o mouse é movido com
  * alguma tecla pressionada */
@@ -604,13 +623,18 @@ void Inicializa (void)
 /* Programa Principal */
 int main(int argc, char **argv)
 {
+
     int j = -8;
      pos_x[0] = 0;
     for(int i = 0; i<16; i++){
         pos_x[i] = j;
         pos_z[i] = -4;
+        color1[i] = 0.8;
+        color2[i] = 0.8;
+        color3[i] = 0.8;
         j++;
     }
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize (LARGURA, ALTURA);
